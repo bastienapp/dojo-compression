179df7c6610d8999b2f5cc593a7acc38c9272cc3
Bastien Krettly <bastien_krettly@wilder.school>
2018-10-02 10:16:33 +0200

    solution

--- solution/Compression.java
+++ solution/Compression.java
@@ -1,31 +1,25 @@
 class Compression {
 
-	public static String compress(String s){
-		int count = 0;
-		char current = ' ';
-		String result = "";
-
-		for(int i = 0; i < s.length(); i++){
-
-			current = s.charAt(i);
-			count = 1;
-
-			for(int j = i+1; j < s.length(); j++) {
-				if (current == s.charAt(j)) {
-					count++;
-					i++;
-				}												
-			}
-			
-			if (count <= 2) {
-				for (int k = 0; k < count ; k++) {
-					result += current;
-				}	
-			} else {
-				result += String.valueOf(count) + current;
-			}	
-		}
-
-		return result;
+	public static String compress(String str) {
+	    String result = "";
+	    int cpt = 1;
+	    
+	    for(int i = 0; i < str.length(); i++) {
+	        if (i + 1 == str.length() || str.charAt(i) != str.charAt(i + 1)) {
+	            if (cpt > 2) {
+	                result += String.valueOf(cpt);	                
+	            } else if (cpt == 2) {
+	                result += str.charAt(i);
+	            }
+	            result += str.charAt(i);
+	        
+	            cpt = 1;
+	        } else {
+	            cpt++;
+	        
+	        }	            
+	    }
+	    
+	    return result;
 	}
 }
